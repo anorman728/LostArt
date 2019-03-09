c28783ca1584cc4c1ff084f886dfc9a365ce2fb5
Andrew Norman <anorman728@gmail.com>
2019-03-09 03:51:34 -0600

    Added a new version of convert_to_wav.

---
+++ convert_to_wav.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+# Simple helper script to convert midi file to wav using fluidsynth, but uses
+# the SETTING__SOUND_FONT in the default_settings.py file.
+
+# Source common settings.
+exec(open("./default_settings.py").read())
+
+argv = sys.argv
+
+os.system('fluidsynth -F "' + argv[1] + '.wav" ' + SETTING__SOUND_FONT + ' ' + argv[1])
